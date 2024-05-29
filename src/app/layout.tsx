b60ce9a5d6f886f4180ed3699a3781e0e1ee649c
Adamu Jethro <jethroadamzy@gmail.com>
2024-05-30 00:18:47 +0100

    okay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,10 @@
 "use client";
-
-import Footer from "@/components/Footer";
 import Header from "@/components/Header";
 import ScrollToTop from "@/components/ScrollToTop";
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider } from "next-themes";
 import "../styles/index.css";
 import "../styles/prism-vsc-dark-plus.css";
-import ToasterContext from "./api/contex/ToasetContex";
 import { useEffect, useState } from "react";
 import PreLoader from "@/components/Common/PreLoader";
 
@@ -40,7 +37,6 @@ export default function RootLayout({
               enableSystem={false}
               defaultTheme="light"
             >
-              <ToasterContext />
               <Header />
               {children}
               <ScrollToTop />
